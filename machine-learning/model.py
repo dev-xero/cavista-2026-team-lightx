319cf00d1861bc07ed6ac7190b482f6ff77a277d
dev-xero <ssh.xero@gmail.com>
2026-02-21 15:11:16 +0100

    feat(ml): configure logging

--- machine-learning/model.py
+++ machine-learning/model.py
@@ -5,13 +5,19 @@ import os
 import warnings
 
 warnings.filterwarnings('ignore')
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
 
 DATA_DIRECTORY="./NHANES_data"
 OUTPUT_DIRECTORY="./output"
 RANDOM_STATE=42
 TEST_SIZE=0.2
 
+
 def make_output_dirs():
+    logger.info("Creating output directory")
     os.makedirs(OUTPUT_DIRECTORY, exist_ok=True)
    
    
@@ -23,24 +29,56 @@ def load_xpt(filename):
         return pd.DataFrame()
         
     df = pd.read_sas(path, format='xport', encoding='utf-8')
+    return df
    
    
 def load_dataset():
     logger.info("Attempting to load all .xpt files")
     
-    bp    = load_xpt("P_BPX.xpt")      # Blood pressure readings
-    demo  = load_xpt("P_DEMO.xpt")     # Demographics
-    bmi   = load_xpt("P_BMX.xpt")      # Body measures
-    chol  = load_xpt("P_TCHOL.xpt")    # Total cholesterol
-    hdl   = load_xpt("P_HDL.xpt")      # HDL cholesterol
-    glu   = load_xpt("P_GLU.xpt")      # Blood glucose
-    smoke = load_xpt("P_SMQ.xpt")      # Smoking
-    kidney= load_xpt("P_BIOPRO.xpt")   # Kidney function
+    bp    = load_xpt("P_BPXO.XPT")     # Blood pressure readings
+    demo  = load_xpt("P_DEMO.XPT")     # Demographics
+    bmi   = load_xpt("P_BMX.XPT")      # Body measures
+    chol  = load_xpt("P_TCHOL.XPT")    # Total cholesterol
+    hdl   = load_xpt("P_HDL.XPT")      # HDL cholesterol
+    glu   = load_xpt("P_GLU.XPT")      # Blood glucose
+    smoke = load_xpt("P_SMQ.XPT")      # Smoking
+    kidney= load_xpt("P_BIOPRO.XPT")   # Kidney function
     
     logger.info("Loaded datasets")
     
     return bp, demo, bmi, chol, hdl, glu, smoke, kidney
     
+
+def combine_datasets(bp, demo, bmi, chol, hdl, glu, smoke, kindey):
+    logger.info("Attempting to combine all datasets")
     
+    if not bp.empty:
+        logger.info("Loading blood pressure")
+        
+        bp_cols_sys = [c for c in ['BPXSY1','BPXSY2','BPXSY3'] if c in bp.columns]
+        bp_cols_dia = [c for c in ['BPXDI1','BPXDI2','BPXDI3'] if c in bp.columns]
+        bp['SYSTOLIC']  = bp[bp_cols_sys].mean(axis=1)
+        bp['DIASTOLIC'] = bp[bp_cols_dia].mean(axis=1)
+        bp = bp[['SEQN', 'SYSTOLIC', 'DIASTOLIC']]
+    
+    if not demo.empty:
+            logger.info("Loading demographic dataset")
+            
+            demo = demo[['SEQN', 'RIDAGEYR', 'RIAGENDR', 'RIDRETH3']].rename(columns={
+                'RIDAGEYR': 'AGE',
+                'RIAGENDR': 'GENDER',
+                'RIDRETH3': 'ETHNICITY'
+            })
+
+
 def train():
    pass
+
+
+def main():
+    make_output_dirs()
+    bp, demo, bmi, chol, hdl, glu, smoke, kidney = load_dataset()
+    combine_datasets(bp, demo, bmi, chol, hdl, glu, smoke, kidney)
+
+
+main()
