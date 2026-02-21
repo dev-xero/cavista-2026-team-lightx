8b1df4ceb64def449b157ffbd7f2194bedfb7a8d
dev-xero <ssh.xero@gmail.com>
2026-02-21 15:27:10 +0100

    feat(ml): Extract useful features from datasets

--- machine-learning/model.py
+++ machine-learning/model.py
@@ -1,4 +1,3 @@
-from PIL.Image import logger
 import pandas as pd
 import logging
 import os
@@ -17,7 +16,7 @@ TEST_SIZE=0.2
 
 
 def make_output_dirs():
-    logger.info("Creating output directory")
+    logging.info("Creating output directory")
     os.makedirs(OUTPUT_DIRECTORY, exist_ok=True)
    
    
@@ -33,7 +32,7 @@ def load_xpt(filename):
    
    
 def load_dataset():
-    logger.info("Attempting to load all .xpt files")
+    logging.info("Attempting to load all .xpt files")
     
     bp    = load_xpt("P_BPXO.XPT")     # Blood pressure readings
     demo  = load_xpt("P_DEMO.XPT")     # Demographics
@@ -44,31 +43,69 @@ def load_dataset():
     smoke = load_xpt("P_SMQ.XPT")      # Smoking
     kidney= load_xpt("P_BIOPRO.XPT")   # Kidney function
     
-    logger.info("Loaded datasets")
+    logging.info("Loaded datasets")
     
     return bp, demo, bmi, chol, hdl, glu, smoke, kidney
     
 
-def combine_datasets(bp, demo, bmi, chol, hdl, glu, smoke, kindey):
-    logger.info("Attempting to combine all datasets")
+def combine_datasets(bp, demo, bmi, chol, hdl, glu, smoke, kidney):
+    logging.info("Attempting to combine all datasets")
     
     if not bp.empty:
-        logger.info("Loading blood pressure")
+        logging.info("Loading blood pressure")
         
         bp_cols_sys = [c for c in ['BPXSY1','BPXSY2','BPXSY3'] if c in bp.columns]
         bp_cols_dia = [c for c in ['BPXDI1','BPXDI2','BPXDI3'] if c in bp.columns]
+        
         bp['SYSTOLIC']  = bp[bp_cols_sys].mean(axis=1)
         bp['DIASTOLIC'] = bp[bp_cols_dia].mean(axis=1)
         bp = bp[['SEQN', 'SYSTOLIC', 'DIASTOLIC']]
+        
+        print(demo, "\n\n")
     
     if not demo.empty:
-            logger.info("Loading demographic dataset")
-            
-            demo = demo[['SEQN', 'RIDAGEYR', 'RIAGENDR', 'RIDRETH3']].rename(columns={
-                'RIDAGEYR': 'AGE',
-                'RIAGENDR': 'GENDER',
-                'RIDRETH3': 'ETHNICITY'
-            })
+        logging.info("Loading Demographic dataset")
+        demo = demo[['SEQN', 'RIDAGEYR', 'RIAGENDR', 'RIDRETH3']].rename(columns={
+            'RIDAGEYR': 'AGE',
+            'RIAGENDR': 'GENDER',
+            'RIDRETH3': 'ETHNICITY'
+        })
+        print(demo, "\n\n")
+    
+    if not bmi.empty:
+        logging.info("Loading BMI dataset")
+        bmi = bmi[['SEQN', 'BMXBMI', 'BMXWAIST']].rename(columns={
+            'BMXBMI':   'BMI',
+            'BMXWAIST': 'WAIST_CM'
+        })
+        print(bmi, "\n\n")
+        
+    if not chol.empty:
+        logging.info("Loading Total Cholesterol dataset")
+        chol = chol[['SEQN', 'LBXTC']].rename(columns={'LBXTC': 'TOTAL_CHOLESTEROL'})
+        print(chol, "\n\n")
+
+    if not hdl.empty:
+        logging.info("Loading HDL Cholesterol dataset")
+        hdl = hdl[['SEQN', 'LBDHDD']].rename(columns={'LBDHDD': 'HDL_CHOLESTEROL'})
+        print(hdl, "\n\n")
+
+    if not glu.empty:
+        logging.info("Loading Glucose dataset")
+        glu = glu[['SEQN', 'LBXGLU']].rename(columns={'LBXGLU': 'FASTING_GLUCOSE'})
+        print( glu, "\n\n")
+
+    if not smoke.empty and 'SMQ020' in smoke.columns:
+        logging.info("Loading Smoking dataset")
+        smoke = smoke[['SEQN', 'SMQ020']].rename(columns={'SMQ020': 'EVER_SMOKED'})
+        # This just encodes smoking data, 1=YES, 2=NO
+        smoke['EVER_SMOKED'] = smoke['EVER_SMOKED'].map({1: 1, 2: 0})
+        print(smoke, "\n\n")
+
+    if not kidney.empty and 'LBXSCR' in kidney.columns:
+        logging.info("Loading Kidney dataset")
+        kidney = kidney[['SEQN', 'LBXSCR']].rename(columns={'LBXSCR': 'CREATININE'})
+        print(kidney, "\n\n")
 
 
 def train():
